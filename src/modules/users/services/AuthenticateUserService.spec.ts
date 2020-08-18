cd312085de4d6a588ab888a1ec50723ab06fe175
sirmcclay <mcclay.design@gmail.com>
2020-08-18 12:38:16 -0300

    Password recover test - continue

--- src/modules/users/services/AuthenticateUserService.spec.ts
+++ src/modules/users/services/AuthenticateUserService.spec.ts
@@ -42,7 +42,7 @@ describe('AuthenticateUser', () => {
       fakeHashProvider,
     );
 
-    expect(
+    await expect(
       authenticateUser.execute({
         email: 'johndoe@example.com',
         password: '132456',
@@ -69,7 +69,7 @@ describe('AuthenticateUser', () => {
       password: '132456',
     });
 
-    expect(
+    await expect(
       authenticateUser.execute({
         email: 'johndoe@example.com',
         password: 'wrong-password',
