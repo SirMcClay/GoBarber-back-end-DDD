203538065c242572601eaf9d6582b2572e5ddec2
sirmcclay <mcclay.design@gmail.com>
2020-08-28 11:59:50 -0300

    chore:Routes data validation

--- src/modules/users/infra/http/routes/sessions.routes.ts
+++ src/modules/users/infra/http/routes/sessions.routes.ts
@@ -1,10 +1,20 @@
 import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
 
 import SessionsController from '../controllers/SessionsController';
 
 const sessionsRouter = Router();
 const sessionController = new SessionsController();
 
-sessionsRouter.post('/', sessionController.create);
+sessionsRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+    },
+  }),
+  sessionController.create,
+);
 
 export default sessionsRouter;
