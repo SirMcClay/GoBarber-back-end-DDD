b0873b9423fc14a6143cdd4654d3931d2a6a3caa
sirmcclay <mcclay.design@gmail.com>
2020-09-02 09:36:39 -0300

    feat:AWS S3 file upload - S3StorageProvider

--- src/shared/container/providers/StorageProvider/index.ts
+++ src/shared/container/providers/StorageProvider/index.ts
@@ -3,9 +3,11 @@ import { container } from 'tsyringe';
 import IStorageProvider from './models/IStorageProvider';
 
 import diskStorageProvider from './implementations/DiskStorageProvider';
+import S3StorageProvider from './implementations/S3StorageProvider';
 
 const providers = {
   disk: diskStorageProvider,
+  s3: S3StorageProvider,
 };
 
 container.registerSingleton<IStorageProvider>(
